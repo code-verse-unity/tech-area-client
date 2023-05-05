738335225f4114c8bcae0cf026786eff994446e7
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-05-05 17:03:47 +0300

    chore : remove unused components

--- src/components/Button/index.ts
+++
@@ -1,3 +0,0 @@
-// components/Button/index.ts
-export { default } from "./Button";
-export * from "./types";
--- src/components/Button/types.ts
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export interface ButtonProps extends PropsWithChildren {
-  text: string;
-  onClick: () => void;
-  disabled?: boolean;
-  className?: string;
-}
