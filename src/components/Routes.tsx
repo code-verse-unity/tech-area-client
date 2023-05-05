f0c0c7ca7da06310b63824d0f6e5c2e7a45c464c
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-05-05 17:03:02 +0300

    feat : configure routes

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,12 +1,31 @@
 import { Route, Routes as ReactRoutes, useLocation } from "react-router-dom";
-import Home from "./Home";
+import RootLayout from "@/layouts/RootLayout";
+import HomePage from "@/features/question/pages/HomePage";
 
 const Routes = () => {
   const location = useLocation();
 
   return (
     <ReactRoutes location={location} key={location.key}>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<RootLayout />}>
+        <Route path="" element={<HomePage />} />
+        <Route path="users" element={<HomePage />} />
+        <Route path="users/:userId" element={<HomePage />} />
+        <Route path="tags" element={<HomePage />} />
+        <Route path="questions" element={<HomePage />} />
+        <Route path="questions/:questionId" element={<HomePage />} />
+        <Route path="events" element={<HomePage />} />
+        <Route path="challenges" element={<HomePage />} />
+        <Route path="profile" element={<HomePage />} />
+      </Route>
+
+      <Route path="/auth" element={<HomePage />}>
+        <Route path="login" element={<HomePage />} />
+        <Route path="register" element={<HomePage />} />
+        <Route path="forgot-password" element={<HomePage />} />
+        <Route path="reset-password" element={<HomePage />} />
+      </Route>
+      <Route path="*" element={<HomePage />} />
     </ReactRoutes>
   );
 };
