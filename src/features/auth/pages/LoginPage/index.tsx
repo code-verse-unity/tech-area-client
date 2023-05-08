73c6fdbaf507c4ed0a33e07fce35d1ead8c7d00c
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-05-08 20:26:16 +0300

    chore : remove unused folder

--- src/features/auth/pages/LoginPage/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Login.module.css";
-
-const LoginPage = () => {
-  return <h1>I am the login page</h1>;
-};
-
-export default LoginPage;
--- src/features/auth/pages/RegisterPage/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Register.module.css";
-
-const RegisterPage = () => {
-  return <h1>I am the register page</h1>;
-};
-
-export default RegisterPage;
