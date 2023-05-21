8eb2722fbb368a590db0b1ac68ef456138b5defe
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-05-21 20:09:55 +0300

    feat : add setUserTags reducer

---
+++ src/hooks/redux.ts
@@ -0,0 +1,7 @@
+import { AppDispatch, RootState } from "@/redux/store";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export type Dispatch = ReturnType<typeof useAppDispatch>;
--- src/redux/reducers/userSlice.ts
+++ src/redux/reducers/userSlice.ts
@@ -1,15 +1,18 @@
-import { User } from "@/utils/types";
+import { Tag, User } from "@/utils/types";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 const initialState: User = {
   id: "",
   firstname: "",
   lastname: "",
+  fullname: "",
   email: "",
+  avatarUrl: "",
   password: "",
   role: "user",
   createdAt: "",
   updatedAt: "",
+  tags: [],
 };
 
 const userSlice = createSlice({
@@ -19,12 +22,18 @@ const userSlice = createSlice({
     setUser: (state, action: PayloadAction<User>) => {
       return action.payload;
     },
+    setUserTags: (state, action: PayloadAction<Tag[]>) => {
+      return {
+        ...state,
+        tags: action.payload,
+      };
+    },
     resetUser: () => {
       return initialState;
     },
   },
 });
 
-export const { setUser, resetUser } = userSlice.actions;
+export const { setUser, setUserTags, resetUser } = userSlice.actions;
 
 export default userSlice.reducer;
